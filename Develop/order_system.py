b5e6992cd1e432046f01cf89b3696f623a89d8f0
logles <lydialogles@gmail.com>
2025-04-19 10:26:09 -0600

    finish print_itemized_recipt function

--- Develop/order_system.py
+++ Develop/order_system.py
@@ -115,37 +115,56 @@ def update_order(order, menu_selection, menu_items):
                     and quantity ordered (updated as needed).
     """
     # TODO: Check if the customer typed a number
+    if menu_selection.isdigit():
 
         # TODO: Convert the menu selection to an integer
+        selection_number = int(menu_selection)
 
 
         # TODO: Check if the menu selection is in the menu items keys
+        if selection_number in menu_items:
 
             # TODO: Store the item name as a variable
-
+            item_name = menu_items[selection_number]["Item name"]
 
             # TODO: Ask the customer for the quantity of the menu item
             # TODO: Use the item name variable in the question
-
+            quantity_input = input(f"How many of '{item_name}' would you like? ")
 
             # TODO: Check if the quantity is a number, default to 1 if not
+            if quantity_input.isdigit():
+                quantity = int(quantity_input)
+            else:
+                print("Invalid quantity. Adding 1 to your order")
+                quantity = 1
 
 
             # TODO: Add a dictionary to the order list 
             # TODO: The dictionary should include the item name, price, and quantity
             # TODO: Use the following names for the dictionary keys:
             # TODO: "Item name", "Price", "Quantity"
+            order.append({
+                "Item name": item_name,
+                "Price": menu_items[selection_number]["Price"],
+                "Quantity": quantity
+            })
 
         # TODO: When the user's input isn't valid, 
         # TODO: tell the customer that their input isn't valid
+        else:
+            print("Sorry, that input isn't valid")
+
 
     # TODO: When the menu selection wasn't valid:
     # TODO: Print the menu selection and 
     # TODO: Tell the customer they didn't select a menu option
+    else:
+        print(f"'{menu_selection}' is not valid. Please select a menu option.")
+
 
 
     # TODO: Return the updated order
-
+    return order
 
 def print_itemized_receipt(receipt):
     """
